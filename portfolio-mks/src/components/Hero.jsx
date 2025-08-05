3de04d3987e9da5f8287eaa8a5c08aad4e17cfe5
abdullhaks <aks4u786@gmail.com>
2025-08-05 21:49:59 +0530

    check

--- portfolio-mks/src/components/Hero.jsx
+++ portfolio-mks/src/components/Hero.jsx
@@ -138,7 +138,7 @@ const Hero = ({ activeSection, setActiveSection }) => {
               >
                 <Heart className="w-5 h-5 mr-2 text-red-500" />
               </motion.div>
-              Trusted by {patients.toString()}+ Patients Worldwide
+              Trusted by 20000+ Patients Worldwide
             </motion.div>
 
             <motion.div
