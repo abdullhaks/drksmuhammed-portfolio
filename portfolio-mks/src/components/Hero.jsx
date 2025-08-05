6af122fe71f1d742d3db0ce844c0850d4d6ead3f
abdullhaks <aks4u786@gmail.com>
2025-08-05 21:21:31 +0530

    refac:count checking

--- portfolio-mks/src/components/Hero.jsx
+++ portfolio-mks/src/components/Hero.jsx
@@ -138,7 +138,7 @@ const Hero = ({ activeSection, setActiveSection }) => {
               >
                 <Heart className="w-5 h-5 mr-2 text-red-500" />
               </motion.div>
-              Trusted by {patients}+ Patients Worldwide
+              Trusted by {patients.toString()}+ Patients Worldwide
             </motion.div>
 
             <motion.div
